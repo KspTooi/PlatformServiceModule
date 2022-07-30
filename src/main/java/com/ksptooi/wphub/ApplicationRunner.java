c23167a1934b4021a9348ed03947ff0ec0b70685
KspTooi <ksptooi@outlook.com>
2022-07-31 05:25:42 +0800

    @

--- src/main/java/com/ksptooi/wphub/ApplicationRunner.java
+++ src/main/java/com/ksptooi/wphub/ApplicationRunner.java
@@ -24,7 +24,6 @@ public class ApplicationRunner {
 
 
         CommandLine cli = injector.getInstance(CommandLine.class);
-
         cli.run();
 
     }
--- src/main/java/com/ksptooi/wphub/cli/OperateCli.java
+++ src/main/java/com/ksptooi/wphub/cli/OperateCli.java
@@ -1,12 +1,55 @@
 package com.ksptooi.wphub.cli;
 
+import com.google.inject.Inject;
+import com.ksptooi.wphub.command.CommandParser;
+import com.ksptooi.wphub.executor.dispatch.CommandScheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class OperateCli implements CommandLine{
+
+
+    private final Logger logger = LoggerFactory.getLogger(OperateCli.class);
+
+    @Inject
+    private CommandScheduler scheduler;
+
+    @Inject
+    private CommandParser parser;
 
 
     @Override
     public void run() {
 
+        logger.info("Cli初始化中");
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("@:");
+
+        while (true){
+
+            try {
+
+
+                String commandString = br.readLine();
+
+                if(commandString.replace(" ","").equals("")){
+                    continue;
+                }
+
+                scheduler.publish(parser.parse(commandString));
+                System.out.print("@:");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
 
