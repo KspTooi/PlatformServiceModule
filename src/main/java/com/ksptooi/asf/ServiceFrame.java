2e1c0e51279d77f0829ce35c591d72b919d03690
KspTooi <ksptooi@outlook.com>
2022-08-01 21:42:07 +0800

    @

--- src/main/java/com/ksptooi/asf/ServiceFrame.java
+++ src/main/java/com/ksptooi/asf/ServiceFrame.java
@@ -3,8 +3,12 @@ package com.ksptooi.asf;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.ksptooi.asf.core.cli.CommandLine;
 import com.ksptooi.asf.core.modules.ApplicationModule;
 import com.ksptooi.asf.core.plugins.PluginLoader;
+import com.ksptooi.asf.core.processor.dispatcher.ProcessorDispatcher;
+import com.ksptooi.asf.extendsbuildin.processor.PackManagerProcessor;
+import com.ksptooi.asf.extendsbuildin.processor.PackRunnerProcessor;
 
 public class ServiceFrame {
 
@@ -20,12 +24,12 @@ public class ServiceFrame {
         epl.install(epl.getPlugin("plugins"));
 
         //注册基本命令
-/*        CommandScheduler scheduler = injector.getInstance(CommandScheduler.class);
-        scheduler.addListener("build-in-PackManagerExecutor",new PackManagerExecutor());
-        scheduler.addListener("build-in-PackRunnerExecutor",new PackRunnerExecutor());
+        ProcessorDispatcher scheduler = injector.getInstance(ProcessorDispatcher.class);
+        scheduler.register("build-in-PackManagerExecutor",new PackManagerProcessor());
+        scheduler.register("build-in-PackRunnerExecutor",new PackRunnerProcessor());
 
         CommandLine cli = injector.getInstance(CommandLine.class);
-        cli.run();*/
+        cli.run();
     }
 
 
--- src/main/java/com/ksptooi/asf/core/modules/ApplicationModule.java
+++ src/main/java/com/ksptooi/asf/core/modules/ApplicationModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 import com.ksptooi.asf.core.cli.CommandLine;
 import com.ksptooi.asf.core.cli.OperateCli;
+import com.ksptooi.asf.core.processor.dispatcher.CmdAnnotationDispatcher;
 import com.ksptooi.asf.core.processor.dispatcher.CmdProcessRegisterWrapper;
 import com.ksptooi.asf.core.processor.dispatcher.ProcessorDispatcher;
 import com.ksptooi.asf.core.plugins.PluginLoader;
@@ -19,7 +20,7 @@ public class ApplicationModule extends AbstractModule {
         //install(new ComponentScanModule("com.ksptooi", Comp.class));
 
         //命令调度器
-        bind(ProcessorDispatcher.class).to(CmdProcessRegisterWrapper.class).in(Scopes.SINGLETON);
+        bind(ProcessorDispatcher.class).to(CmdAnnotationDispatcher.class).in(Scopes.SINGLETON);
 
         //Cli
         bind(CommandLine.class).to(OperateCli.class).in(Scopes.SINGLETON);
