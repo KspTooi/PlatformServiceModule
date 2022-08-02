6d8c1ea6bc810c2055de741ec54f14310359545d
KspTooi <ksptooi@outlook.com>
2022-08-02 17:06:07 +0800

    ProcessorAnnatationScanner

--- src/main/java/com/ksptooi/asf/core/processor/ProcessorAnnotationScanner.java
+++ src/main/java/com/ksptooi/asf/core/processor/ProcessorAnnotationScanner.java
@@ -1,8 +1,10 @@
 package com.ksptooi.asf.core.processor;
 
 import org.reflections.Reflections;
+import org.reflections.util.ConfigurationBuilder;
 
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -15,17 +17,50 @@ public class ProcessorAnnotationScanner implements ProcessorScanner{
 
         Reflections reflections = new Reflections(packagePath);
 
-        Set<Class<?>> processors = reflections.getTypesAnnotatedWith(com.ksptooi.asf.core.annatatiotion.Processor.class);
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(com.ksptooi.asf.core.annatatiotion.Processor.class);
 
-        if(processors.size()<1){
+        return this.getProcessorForClassSet(typesAnnotatedWith);
+    }
+
+    @Override
+    public Map<String, Processor> scan(URL url) {
+
+        ClassLoader loader=new URLClassLoader(new URL[]{url});
+
+        Reflections packageReflections = new Reflections(new ConfigurationBuilder()
+                .addUrls(url).addClassLoaders(loader)
+        );
+
+        Set<Class<?>> typesAnnotatedWith = packageReflections.getTypesAnnotatedWith(com.ksptooi.asf.core.annatatiotion.Processor.class);
+
+        return this.getProcessorForClassSet(typesAnnotatedWith);
+    }
+
+
+    @Override
+    public Map<String, Processor> scan(URL url, ClassLoader classLoader) {
+
+        Reflections packageReflections = new Reflections(new ConfigurationBuilder()
+                .addUrls(url).addClassLoaders(classLoader)
+        );
+
+        Set<Class<?>> typesAnnotatedWith = packageReflections.getTypesAnnotatedWith(com.ksptooi.asf.core.annatatiotion.Processor.class);
+
+        return this.getProcessorForClassSet(typesAnnotatedWith);
+    }
+
+
+    private Map<String,Processor> getProcessorForClassSet(Set<Class<?>> classSet){
+
+        if(classSet.size()<1){
             return new HashMap<>();
         }
 
         Map<String,Processor> retMap =  new HashMap<>();
 
-        while (processors.iterator().hasNext()) {
+        while (classSet.iterator().hasNext()) {
 
-            Class<?> next = processors.iterator().next();
+            Class<?> next = classSet.iterator().next();
 
             try {
 
@@ -41,14 +76,6 @@ public class ProcessorAnnotationScanner implements ProcessorScanner{
         return retMap;
     }
 
-    @Override
-    public Map<String, Processor> scan(URL url) {
-        return null;
-    }
 
-    @Override
-    public Map<String, Processor> scan(URL url, ClassLoader classLoader) {
-        return null;
-    }
 
 }
