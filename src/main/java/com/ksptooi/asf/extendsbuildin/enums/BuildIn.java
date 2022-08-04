53658d45441477e91174fe562be99a3ad49b3cdf
KspTooi <ksptooi@outlook.com>
2022-08-05 00:53:59 +0800

    Build-in Processor Enums

---
+++ src/main/java/com/ksptooi/asf/extendsbuildin/enums/BuildIn.java
@@ -0,0 +1,22 @@
+package com.ksptooi.asf.extendsbuildin.enums;
+
+public enum BuildIn {
+
+    APP("build-in-ApplicationProcessor"),
+    APP_RUNNER("build-in-ApplicationRunnerProcessor");
+
+
+    private String processorName;
+
+    BuildIn(String name) {
+        this.processorName = name;
+    }
+
+    public String getProcessorName() {
+        return processorName;
+    }
+
+    public void setProcessorName(String processorName) {
+        this.processorName = processorName;
+    }
+}
