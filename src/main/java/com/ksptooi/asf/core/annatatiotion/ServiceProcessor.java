57504b1592ad7cd5b218c6401ea1712482befcdb
KspTooi <ksptooi@outlook.com>
2022-08-15 15:16:36 +0800

    ServiceProcessor

---
+++ src/main/java/com/ksptooi/asf/core/annatatiotion/ServiceProcessor.java
@@ -0,0 +1,9 @@
+package com.ksptooi.asf.core.annatatiotion;
+
+/**
+ * Define the "Processor" as a Background Processor
+ * You can only operate on a background process when it is active
+ */
+public interface ServiceProcessor {
+    
+}
