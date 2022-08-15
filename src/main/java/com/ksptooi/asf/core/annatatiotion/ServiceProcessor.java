dc8723d9a6fe4a1a2f4d7ff2bccefc30add7e7ea
KspTooi <ksptooi@outlook.com>
2022-08-15 15:18:34 +0800

    ServiceProcessor

--- src/main/java/com/ksptooi/asf/core/annatatiotion/ServiceProcessor.java
+++ src/main/java/com/ksptooi/asf/core/annatatiotion/ServiceProcessor.java
@@ -1,9 +1,16 @@
 package com.ksptooi.asf.core.annatatiotion;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Define the "Processor" as a Background Processor
  * You can only operate on a background process when it is active
  */
-public interface ServiceProcessor {
-    
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ServiceProcessor {
+
 }
---
+++ src/main/java/com/ksptooi/asf/extendsbuildin/processor/BackgroundProcessor.java
@@ -0,0 +1,12 @@
+package com.ksptooi.asf.extendsbuildin.processor;
+
+import com.ksptooi.asf.core.annatatiotion.Processor;
+import com.ksptooi.asf.core.annatatiotion.ServiceProcessor;
+
+@Processor("build-in-Dev-BackgroundProcessor")
+@ServiceProcessor
+public class BackgroundProcessor {
+
+
+
+}
