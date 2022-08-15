ea46fcee1138b646762a097290c2c183c50a46e8
KspTooi <ksptooi@outlook.com>
2022-08-15 17:34:46 +0800

    @

---
+++ src/main/java/com/ksptooi/asf/core/cli/CliLogger.java
@@ -0,0 +1,13 @@
+package com.ksptooi.asf.core.cli;
+
+import org.slf4j.Logger;
+
+public class CliLogger extends CliLoggerAdaptor{
+
+    public CliLogger(Logger proxyLogger) {
+        super(proxyLogger);
+    }
+
+
+
+}
