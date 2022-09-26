ba0c44418699cb9d1f7479fdc08004701a933ba4
KspTooi <ksptooi@outlook.com>
2022-09-26 11:45:58 +0800

    @

--- src/main/java/com/ksptooi/asf/extendsbuildin/processor/PluginManagerProcessor.java
+++ src/main/java/com/ksptooi/asf/extendsbuildin/processor/PluginManagerProcessor.java
@@ -1,4 +1,28 @@
 package com.ksptooi.asf.extendsbuildin.processor;
 
-public class PluginManagerProcessor {
+import com.ksptooi.asf.core.annatatiotion.Processor;
+import com.ksptooi.asf.core.entities.CliCommand;
+import com.ksptooi.asf.core.entities.Command;
+import com.ksptooi.asf.core.processor.ProcessorAdapter;
+
+
+
+@Processor("build-in-PluginManagerProcessor")
+public class PluginManagerProcessor extends ProcessorAdapter {
+
+
+    @Override
+    public String[] defaultCommand() {
+        return new String[]{
+
+        };
+    }
+
+
+
+
+    @Override
+    public void onCommand(CliCommand preparedCommand, Command command) {
+
+    }
 }
