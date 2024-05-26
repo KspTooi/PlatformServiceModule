e8d564e2a738b917c4d7e2b0cb6d820a0e20a7b0
KspTooi <ksptooi@outlook.com>
2024-05-26 20:45:44 +0800

    ﻿更新:

--- src/main/java/com/ksptooi/psm/processor/TestProcessor.java
+++ src/main/java/com/ksptooi/psm/processor/TestProcessor.java
@@ -1,7 +1,12 @@
 package com.ksptooi.psm.processor;
 
+import com.ksptooi.uac.core.annatatiotion.CommandMapping;
+import com.ksptooi.uac.core.annatatiotion.Param;
+import com.ksptooi.uac.core.entities.CliCommand;
+
 @RequestProcessor("TestProcessor")
 public class TestProcessor implements Processor {
+
 
     @Override
     public void activated() {
@@ -15,6 +20,14 @@ public class TestProcessor implements Processor {
 
     @Override
     public void destroy() {
+
+    }
+
+    @RequestName("install")
+    @Alias({"i"})
+    public void appInstall(@Param("appName")String appName,@Param("path")String path, ProcRequest req){
+
+        
 
     }
 
