ced5843363307146543d815957213017a9540c8f
KspTooi <ksptooi@outlook.com>
2024-06-09 06:49:55 +0800

    ﻿更新:

---
+++ src/main/java/com/ksptooi/psm/configset/ConfigSet.java
@@ -0,0 +1,11 @@
+package com.ksptooi.psm.configset;
+
+public class ConfigSet {
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/ksptooi/psm/processor/event/UserTypingEvent.java
@@ -0,0 +1,32 @@
+package com.ksptooi.psm.processor.event;
+
+import com.ksptooi.psm.processor.event.generic.AbstractProcEvent;
+import com.ksptooi.psm.shell.PSMShell;
+import lombok.Getter;
+
+public class UserTypingEvent extends AbstractProcEvent {
+
+    public final PSMShell userShell;
+
+    @Getter
+    private final char[] chars;
+    @Getter
+    private final int len;
+    @Getter
+    private final String content;
+
+
+
+    public UserTypingEvent(PSMShell shell, char[] chars,int len,String content){
+        this.userShell = shell;
+        this.chars = chars;
+        this.len = len;
+        this.content = content;
+    }
+
+    @Override
+    public PSMShell getUserShell() {
+        return userShell;
+    }
+
+}
