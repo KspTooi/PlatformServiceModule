8c62d6905bedd0aed894a37c5574501ad847406e
KspTooi <ksptooi@outlook.com>
2024-06-10 22:56:28 +0800

    ﻿更新:

---
+++ InfrastructureHubSystem/src/main/java/com/ksptooi/ihub/InfrastructureHubSystem.java
@@ -0,0 +1,29 @@
+package com.ksptooi.ihub;
+
+
+import com.ksptooi.psm.subsystem.SubSystem;
+import com.ksptooi.psm.subsystem.SubSystemEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SubSystemEntry(name = "InfrastructureHubSystem",version = "1.0A")
+public class InfrastructureHubSystem extends SubSystem {
+
+    private final Logger log = LoggerFactory.getLogger(InfrastructureHubSystem.class);
+
+    @Override
+    public void onActivated() {
+
+        System.out.println("""
+                ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░\s
+                ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+                ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+                ░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░\s
+                ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+                ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+                ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░\s""");
+
+    }
+
+
+}
---
+++ InfrastructureHubSystem/src/main/java/com/ksptooi/ihub/processor/UserStatementProcessor.java
@@ -0,0 +1,81 @@
+package com.ksptooi.ihub.processor;
+
+import com.ksptooi.psm.mapper.StatementHistoryMapper;
+import com.ksptooi.psm.mapper.UsersMapper;
+import com.ksptooi.psm.modes.StatementHistoryVo;
+import com.ksptooi.psm.modes.UserVo;
+import com.ksptooi.psm.processor.ProcRequest;
+import com.ksptooi.psm.processor.RequestProcessor;
+import com.ksptooi.psm.processor.event.BadRequestEvent;
+import com.ksptooi.psm.processor.event.StatementCommitEvent;
+import com.ksptooi.psm.processor.hook.EventHandler;
+import com.ksptooi.psm.shell.Colors;
+import jakarta.inject.Inject;
+import org.apache.commons.codec.binary.Hex;
+import xyz.downgoon.snowflake.Snowflake;
+
+import java.util.Date;
+
+@RequestProcessor("bundled:proc:user_statement")
+public class UserStatementProcessor {
+
+    @Inject
+    private StatementHistoryMapper statementHistMapper;
+
+    @Inject
+    private UsersMapper usersMapper;
+
+    @Inject
+    private Snowflake snowflake;
+
+
+    @EventHandler(global = true)
+    public void saveUserStatement(StatementCommitEvent event){
+
+        var session = event.getUserShell().getSession().getSession();
+        var sessionId = session.getSessionId();
+        var account = session.getUsername();
+
+        final UserVo userVo = usersMapper.getByAccount(account);
+        final var statement = event.getStatement();
+
+        StatementHistoryVo i = new StatementHistoryVo();
+        i.setId(snowflake.nextId());
+        i.setUserId(userVo.getUid());
+        i.setUserAccount(userVo.getAccount());
+        i.setStatement(statement);
+        i.setSessionId(Hex.encodeHexString(sessionId));
+        i.setIpAddress(session.getClientAddress().toString());
+        i.setAtTime(new Date());
+        statementHistMapper.insert(i);
+    }
+
+
+    @EventHandler(global = true)
+    public void badRequestNotify(BadRequestEvent event){
+
+        ProcRequest request = event.getRequest();
+        var w = request.getShell().getCable();
+
+        if(event.getErrorCode().equals(BadRequestEvent.ERR_INVOKE_EXCEPTION)){
+            return;
+        }
+
+        if(event.getErrorCode().equals(BadRequestEvent.ERR_HANDLER_TYPE_INCONSISTENT)){
+            w.print("fatal: ");
+            w.print(request.getPattern());
+            w.print(" ");
+            w.print("无法处理请求,处理器与标定的不一致.");
+            w.flush();
+            return;
+        }
+
+        w.print(Colors.RED);
+        w.print(request.getPattern());
+        w.print(": 无法处理请求,匹配处理器失败.");
+        w.print(Colors.RESET);
+        w.flush();
+    }
+
+
+}
