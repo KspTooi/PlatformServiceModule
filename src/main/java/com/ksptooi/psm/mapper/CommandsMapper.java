5851a86a956358010bf4f69d9e790af80b1a6d0d
KspTooi <ksptooi@outlook.com>
2024-07-11 10:31:23 +0800

    CommandMapper

--- src/main/java/com/ksptooi/psm/mapper/CommandsMapper.java
+++ src/main/java/com/ksptooi/psm/mapper/CommandsMapper.java
@@ -9,8 +9,12 @@ import java.util.List;
 @Mapper
 public interface CommandsMapper {
 
+    public int insert(@Param("val")CommandEntity val);
+
     public CommandEntity getByPattern(@Param("val")String val);
 
     public List<CommandEntity> queryMany(@Param("pattern")String pattern);
+
+    public int deleteById(@Param("id")Long id);
 
 }
